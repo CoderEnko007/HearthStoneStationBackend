9a10dc280255ad07393ed7d978ea1eefa39c22d8
fang.yuan <yf381966217@163.com>
2019-02-09 20:14:11 +0800

    modify rank adminx

--- apps/rank/adminx.py
+++ apps/rank/adminx.py
@@ -2,8 +2,8 @@ import xadmin
 from .models import HSRanking
 
 class HSRankingAdmin(object):
-    list_display = ['game_type', 'faction', 'win_rate', 'report_time']
-    list_filter = ['game_type', 'report_time', 'faction']
+    list_display = ['game_type', 'faction', 'win_rate', 'popularity', 'report_time']
+    list_filter = ['game_type', 'faction', 'report_time']
     search_fields = ['game_type', 'faction']
     ordering = ('-win_rate',)
 
